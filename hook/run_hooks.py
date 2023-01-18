9c211b26c2c26970dc3723ec1c9528e40bcd1d92
Guy Edelshtein <guye@dazz.io>
2023-01-18 20:00:53 +0200

    .

--- hook/run_hooks.py
+++ hook/run_hooks.py
@@ -1,10 +1,34 @@
-import black
+import os
+import sys
+
 from click import echo
-import pylint
+import black
+from isort.main import main as isort_main
+from pylint.lint import Run as PylintRun
 
 
-def main():
-    echo("Running black")
-    black.patched_main()
+def _get_config_file(filename: str) -> str:
+    """Get the config file path."""
+    return os.path.join(os.path.dirname(__file__), "configs", filename)
+
+
+def main(folder: str) -> None:
+    echo("Running isort")
+    isort_main([folder, "--settings-path", _get_config_file("isort.cfg"), "--check"])
+
     echo("Running pylint")
-    pylint.run_pylint()
+    x = PylintRun([folder, "--rcfile", _get_config_file("pylintrc")], exit=False)
+    if x.linter.msg_status:
+        sys.exit(x.linter.msg_status)
+
+    x = 5
+
+    # Black must be last, because it's calling sys.exit() on its own
+    echo("Running black")
+    black.main([folder])
+
+
+if __name__ == "__main__":
+    sys.argv.append(".")
+    echo(sys.argv)
+    main(sys.argv[1])
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
 
 from setuptools import setup
+
 setup(install_requires=open("./reqs_for_setup.txt").readlines())
